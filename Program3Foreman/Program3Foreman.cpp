81bd384b39b2447e706986477bcc2503e4fa24b9
keats <243628717+KeatsForeman@users.noreply.github.com>
2026-06-11 00:24:32 -0400

    replaced graphics with correct images

--- Program3Foreman/Program3Foreman.cpp
+++ Program3Foreman/Program3Foreman.cpp
@@ -14,8 +14,8 @@ int main(int argc, char** argv){
     const float fps = 60;
     const int screenW = 600;
     const int screenH = 900;
-    const int frogW = 50;
-    const int frogH = 100;
+    const int fishW = 160;
+    const int fishH = 200;
     float angle = 0;
     bool done = false;
     bool redraw = false;
@@ -28,7 +28,8 @@ int main(int argc, char** argv){
     ALLEGRO_BITMAP* image = NULL;
     ALLEGRO_BITMAP* fly1 = NULL;
     ALLEGRO_BITMAP* fly2 = NULL;
-    ALLEGRO_BITMAP* frog = NULL;
+    ALLEGRO_BITMAP* fish = NULL;
+    ALLEGRO_BITMAP* spittle = NULL;
 
     if (!al_init()) {
         return -1;
@@ -36,10 +37,11 @@ int main(int argc, char** argv){
 
     al_init_image_addon();
 
-    image = al_load_bitmap("fantasy.png");
-    frog = al_load_bitmap("arrow1.png");
-    fly1 = al_load_bitmap("attacking.png");
-
+    image = al_load_bitmap("waterImage.png");
+    fish = al_load_bitmap("fish.png");
+    fly1 = al_load_bitmap("fly.png");
+    fly2 = al_load_bitmap("flysad.png");
+    spittle = al_load_bitmap("spittle.png");
 
 
     display = al_create_display(screenW, screenH);
@@ -64,10 +66,18 @@ int main(int argc, char** argv){
             redraw = true;
             float temp;
 
-            if (keys[LEFT])
+            if (keys[LEFT]) {
+                temp = angle;
                 angle -= .1;
-            if (keys[RIGHT])
+                if (angle < -ALLEGRO_PI / 3)
+                    angle = temp;
+            }
+            if (keys[RIGHT]) {
+                temp = angle;
                 angle += .1;
+                if (angle > ALLEGRO_PI / 3)
+                    angle = temp;
+            }
         }
 
         else if (ev.type == ALLEGRO_EVENT_KEY_DOWN)
@@ -123,9 +133,11 @@ int main(int argc, char** argv){
         if (redraw && al_is_event_queue_empty(event_queue)) {
             al_clear_to_color(al_map_rgb(0, 0, 0));
             al_draw_bitmap(image, 0, 0, 0);
-            //al_draw_bitmap(frog, 400, 800, 0);
-            al_draw_rotated_bitmap(frog, (frogW / 2), frogH, (screenW / 2), 800, angle, 0);
+            //al_draw_bitmap(fish, 400, 800, 0);
+            al_draw_rotated_bitmap(fish, (fishW / 2), fishH, (screenW / 2), 800, angle, 0);
             al_draw_bitmap(fly1, 300, screenH / 2, 0);
+            al_draw_bitmap(fly2, 200, screenH / 2, 0);
+            al_draw_bitmap(spittle, screenW / 2, screenH / 2, 0);
             al_flip_display();
             redraw = false;
         }
